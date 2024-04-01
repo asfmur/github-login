256388199dc9fa68dce2f7aab3fa1aecd6a2af23
asfmur <100413137+asfmur@users.noreply.github.com>
2024-04-01 20:10:38 -0300

    DWII 02/04/2024
    Demonstração de um recurso protegido por uma ferramenta que utiliza OAuth2.0
    Para acessar o recurso o usuário precisa realizar login em uma Conta do Github.

--- src/main/java/com/oauth2/githublogin/controller/HomeController.java
+++ src/main/java/com/oauth2/githublogin/controller/HomeController.java
@@ -1,23 +1,14 @@
 package com.oauth2.githublogin.controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController 
 public class HomeController {
-	
-	@GetMapping("/")
-	public String home() {
-		
-		return "Voce esta na pagina inicial de acesso livre";
-		
-	}
-	
-	@GetMapping("/PaginaDoRecurso")
-	public String recurso() {
-		
-		return "Voce esta na pagina do recurso privado e entro por meio do lo gin github";
-		
-	}
-	
 
+	@GetMapping("/")
+	public String home() {return "Pagina pode ser acessada sem Oauth2";}
+	
+	@GetMapping("/recurso")
+	public String recurso() { return "Pagina do recurso so sera acessada apos o login";}
 }
